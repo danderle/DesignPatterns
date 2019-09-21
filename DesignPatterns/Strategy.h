cce5b94c667f0ceeeab8cf0c286bd8c300827737
danderle <danderle86@gmail.com>
2019-09-21 08:18:21 +0200

    Strategy pattern example

--- DesignPatterns/Strategy.h
+++ DesignPatterns/Strategy.h
@@ -1,72 +1,85 @@
 #pragma once
 #include <memory>
 
+//Abstract mood class
 class Mood
 {
 public:
-	virtual ~Mood() = default;
-	virtual void MyMood() = 0;
+	//Derived mood classes will override this
+	virtual void MyMood() const = 0;
 };
 
+typedef std::shared_ptr<Mood> moodPtr;
+
+//Derived sad class
 class Sad : public Mood
 {
 public:
-	virtual void MyMood() override;
+	//prints the sad mood
+	void MyMood() const override
+	{
+		std::cout << "I am sad\n";
+	}
 };
 
+//Derived happy class
 class Happy : public Mood
 {
 public:
-	virtual void MyMood() override;
+	//prints the happy mood
+	void MyMood() const override
+	{
+		std::cout << "I am happy\n";
+	}
+
 };
 
+//Derived stressed class
 class Stressed : public Mood
 {
 public:
-	virtual void MyMood() override;
+	void MyMood() const override
+	{
+		std::cout << "I am stressed\n";
+	}
 };
 
+//Human class
 class Human
 {
 public:
-	Human() = default;
-	~Human();
-	void setMyMood(std::shared_ptr<Mood> mood);
-	void printMood() const;
+	//Sets the mood of the human
+	void setMyMood(std::shared_ptr<Mood> mood)
+	{
+		pMyMood = mood;
+	}
+
+	//Prints the current mood
+	void printMood() const
+	{
+		pMyMood->MyMood();
+	}
+
 private:
 	std::shared_ptr<Mood> pMyMood;
 };
 
-#include "Strategy.h"
-#include <iostream>
-
-
-
-void Sad::MyMood()
-{
-	std::cout << "I am sad\n";
-}
-
-void Happy::MyMood()
-{
-	std::cout << "I am happy\n";
-}
-
-void Stressed::MyMood()
-{
-	std::cout << "I am stressed\n";
-}
-
-void Human::setMyMood(std::shared_ptr<Mood> mood)
-{
-	pMyMood = mood;
-}
-
-void Human::printMood() const
-{
-	pMyMood->MyMood();
-}
-
-Human::~Human()
-{
-}
+//
+////Example of the strategy pattern
+//int main()
+//{
+//	Human h1;
+//	Human h2;
+//
+//	Mood* mood = new Happy();
+//	h1.setMyMood((moodPtr)mood);
+//	h2.setMyMood((moodPtr)mood);
+//	h1.printMood();
+//	h2.printMood();
+//
+//	mood = new Stressed();
+//	h2.setMyMood((moodPtr)mood);
+//	h2.printMood();
+//
+//	return 0;
+//}
